9aa5b770a8a5faeaa6b161da8422df7305e9bdc2
Daniel Varela <daniel38912@gmail.com>
2025-02-18 15:14:18 -0600

    chore: update formatting, misc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./components/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'gallery',
+    loadComponent: () =>
+      import('./components/gallery/gallery.component').then(
+        (c) => c.GalleryComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
